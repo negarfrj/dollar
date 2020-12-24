f15349e688c95b47038b08b82262bed6d97c8fd8
negarfrj <ngr.faraji@gmail.com>
2020-12-24 18:16:44 +0330

    master

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const api = { base : 'https://gist.githubusercontent.com/ahmadpanah/e6225b11235a04397b813d0de6e6f5ad/raw/ab559a1146b5e6f2d57bd546c20c090842d5fcc6/sana.json'}
+
+function App() {
+  return (
+    <div className="App">
+      <main> 
+        <div className = "box">
+
+          </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
